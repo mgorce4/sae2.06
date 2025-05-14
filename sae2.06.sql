4cc4f86dffe51175a0e5d68ebaec6aa2d941867f
Max Gorce <max.gorce@yahoo.com>
2025-05-14 16:29:19 +0200

    add 2 exs

--- sae2.06.sql
+++ sae2.06.sql
@@ -2,7 +2,7 @@
 select ROUND(AVG(subtotal),2) as average from orders;
 
 --moyenne2
-select ROUND(AVG(subtotal),2) as average 
+select category, ROUND(AVG(subtotal),2) as average 
 from orders o join products p on o.product_id=p.product_id join clients c on o.client_id = c.client_id
 GROUP BY category
 ORDER BY average desc, category asc;
@@ -15,3 +15,19 @@ select o.order_id, o.date, p.name, p.category, o.subtotal
 from orders o join products p on o.product_id=p.product_id join clients c on o.client_id = c.client_id  
 WHERE subtotal >= (SELECT average FROM avgTotal)
 order by order_id;
+
+--mention
+WITH avgTotal AS (
+    SELECT ROUND(AVG(subtotal), 2) AS average FROM orders
+)
+SELECT 
+    o.order_id, 
+    o.subtotal,
+    CASE 
+        WHEN o.subtotal >= (SELECT average FROM avgTotal) THEN 'Bonne'
+        ELSE 'Mauvais'
+    END AS case
+FROM orders o
+JOIN products p ON o.product_id = p.product_id
+JOIN clients c ON o.client_id = c.client_id
+ORDER BY o.order_id;
